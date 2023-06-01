ecea87d31a188f237663386bab09df30a6384282
Nataliia Antonova <5555482@gmail.com>
2023-06-01 12:02:25 +0100

    Added conditional homepage content

--- app/Main.js
+++ app/Main.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -8,13 +8,21 @@ import HomeGuest from "./components/HomeGuest";
 import Footer from "./components/Footer";
 import About from "./components/About";
 import Terms from "./components/Terms";
+import Home from "./components/Home";
 
 function Main() {
+  const [loggedIn, setLoggedIn] = useState(
+    Boolean(localStorage.getItem("onefourtalkToken"))
+  );
   return (
     <BrowserRouter>
-      <Header />
+      <Header loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
       <Routes>
-        <Route path="/" element={<HomeGuest />} />
+        {loggedIn ? (
+          <Route path="/" element={<Home />} />
+        ) : (
+          <Route path="/" element={<HomeGuest />} />
+        )}
         <Route path="/about-us" element={<About />} />
         <Route path="/terms" element={<Terms />} />
       </Routes>
