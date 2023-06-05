d3a86c5bc8a0f8f13e06a1ac73d97748c9945e6f
Nataliia Antonova <5555482@gmail.com>
2023-06-05 10:37:32 +0100

    useReducer & Context

--- app/components/HeaderLoggedOut.js
+++ app/components/HeaderLoggedOut.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState, useContext } from "react";
 import Axios from "axios";
-import ExampleContext from "../ExampleContext";
+import DispatchContext from "../DispatchContext";
 
 function HeaderLoggedOut(props) {
-  const { setLoggedIn } = useContext(ExampleContext);
+  const appDispatch = useContext(DispatchContext);
   const [username, setUsername] = useState();
   const [password, setPassword] = useState();
 
@@ -15,7 +15,7 @@ function HeaderLoggedOut(props) {
         localStorage.setItem("onefourtalkToken", response.data.token);
         localStorage.setItem("onefourtalkUsername", response.data.username);
         localStorage.setItem("onefourtalkAvatar", response.data.avatar);
-        setLoggedIn(true);
+        appDispatch({ type: "login" });
       } else {
         console.log("Incorrect username / password.");
       }
