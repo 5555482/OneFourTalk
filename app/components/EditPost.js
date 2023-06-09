d1129f83f3b3124d5c3ec6f3c29dc59c2f145527
Nataliia Antonova <5555482@gmail.com>
2023-06-09 14:54:00 +0100

    Client side validation

--- app/components/EditPost.js
+++ app/components/EditPost.js
@@ -36,19 +36,35 @@ function ViewSinglePost() {
         draft.isFetching = false;
         return;
       case "titleChange":
+        draft.title.hasErrors = false;
         draft.title.value = action.value;
         return;
       case "bodyChange":
+        draft.body.hasErrors = false;
         draft.body.value = action.value;
         return;
       case "submitRequest":
-        draft.sendCount++;
+        if (!draft.title.hasErrors && !draft.body.hasErrors) {
+          draft.sendCount++;
+        }
         return;
       case "saveRequestStarted":
         draft.isSaving = true;
         return;
       case "saveRequestFinished":
         draft.isSaving = false;
+        return;
+      case "titleRules":
+        if (!action.value.trim()) {
+          draft.title.hasErrors = true;
+          draft.title.message = "You must provide a title.";
+        }
+        return;
+      case "bodyRules":
+        if (!action.value.trim()) {
+          draft.body.hasErrors = true;
+          draft.body.message = "You must provide body content.";
+        }
         return;
     }
   }
@@ -57,6 +73,8 @@ function ViewSinglePost() {
 
   function submitHandler(e) {
     e.preventDefault();
+    dispatch({ type: "titleRules", value: state.title.value });
+    dispatch({ type: "bodyRules", value: state.body.value });
     dispatch({ type: "submitRequest" });
   }
 
@@ -121,6 +139,9 @@ function ViewSinglePost() {
             <small>Title</small>
           </label>
           <input
+            onBlur={e =>
+              dispatch({ type: "titleRules", value: e.target.value })
+            }
             onChange={e =>
               dispatch({ type: "titleChange", value: e.target.value })
             }
@@ -133,6 +154,11 @@ function ViewSinglePost() {
             placeholder=""
             autoComplete="off"
           />
+          {state.title.hasErrors && (
+            <div className="alert alert-danger small liveValidateMessage">
+              {state.title.message}
+            </div>
+          )}
         </div>
 
         <div className="form-group">
@@ -140,6 +166,7 @@ function ViewSinglePost() {
             <small>Body Content</small>
           </label>
           <textarea
+            onBlur={e => dispatch({ type: "bodyRules", value: e.target.value })}
             onChange={e =>
               dispatch({ type: "bodyChange", value: e.target.value })
             }
@@ -149,6 +176,11 @@ function ViewSinglePost() {
             type="text"
             value={state.body.value}
           />
+          {state.body.hasErrors && (
+            <div className="alert alert-danger small liveValidateMessage">
+              {state.body.message}
+            </div>
+          )}
         </div>
 
         <button className="btn btn-primary" disabled={state.isSaving}>
