6edcb0368be407ac225a76210f0d18aa09078ae8
Nataliia Antonova <5555482@gmail.com>
2023-06-16 15:20:18 +0100

    chat icon updates

--- app/components/HeaderLoggedIn.js
+++ app/components/HeaderLoggedIn.js
@@ -11,53 +11,58 @@ function HeaderLoggedIn(props) {
   function handleLogout() {
     appDispatch({ type: "logout" });
   }
-  function handleSearch(e) {
+
+  function handleSearchIcon(e) {
     e.preventDefault();
     appDispatch({ type: "openSearch" });
   }
+
   return (
     <div className="flex-row my-3 my-md-0">
       <a
-        onClick={handleSearch}
-        href="#"
-        className="text-white mr-2 ml-2 header-search-icon"
         data-for="search"
         data-tip="Search"
+        onClick={handleSearchIcon}
+        href="#"
+        className="text-white mr-2 header-search-icon"
       >
         <i className="fas fa-search"></i>
       </a>
-      <ReactTooltip place="bottom" id="search" className="custom-tooltip" />
+      <ReactTooltip place="bottom" id="search" className="custom-tooltip" />{" "}
       <span
         onClick={() => appDispatch({ type: "toggleChat" })}
         data-for="chat"
         data-tip="Chat"
-        className="header-chat-icon text-white mr-2 ml-2"
+        className={
+          "mr-2 header-chat-icon " +
+          (appState.unreadChatCount ? "text-danger" : "text-white")
+        }
       >
         <i className="fas fa-comment"></i>
-        <span className="chat-count-badge text-white"> </span>
+        {appState.unreadChatCount ? (
+          <span className="chat-count-badge text-white">
+            {appState.unreadChatCount < 10 ? appState.unreadChatCount : "9+"}
+          </span>
+        ) : (
+          ""
+        )}
       </span>
-      <ReactTooltip place="bottom" id="chat" className="custom-tooltip" />
+      <ReactTooltip place="bottom" id="chat" className="custom-tooltip" />{" "}
       <Link
         data-for="profile"
-        data-tip="My profile"
-        className="mr-2 ml-2"
+        data-tip="My Profile"
         to={`/profile/${appState.user.username}`}
+        className="mr-2"
       >
         <img className="small-header-avatar" src={appState.user.avatar} />
       </Link>
-      <ReactTooltip place="bottom" id="profile" className="custom-tooltip" />
-      <Link
-        className="inline-block text-sm px-4 py-2 mr-2 ml-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
-        to="/create-post"
-      >
+      <ReactTooltip place="bottom" id="profile" className="custom-tooltip" />{" "}
+      <Link className="btn btn-sm btn-success mr-2" to="/create-post">
         Create Post
-      </Link>
-      <Link
-        onClick={handleLogout}
-        className="inline-block text-sm px-4 py-2 mr-2 ml-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
-      >
+      </Link>{" "}
+      <button onClick={handleLogout} className="btn btn-sm btn-secondary">
         Sign Out
-      </Link>
+      </button>
     </div>
   );
 }
